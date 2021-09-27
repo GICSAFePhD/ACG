157b9a680de02207648b5024494bd236088f96de
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-27 15:51:46 -0300

    Main function created

---
+++ ACG.py
@@ -0,0 +1,5 @@
+def ACG():
+    print("#"*20+" Starting Automatic Code Generator "+"#"*20)
+    print("Reading railML object")
+    print("Generating interlocking table")
+    print("Generating VHDL code")
